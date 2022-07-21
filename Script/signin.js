20089d80817aabfa1986ad9ed2a486f020f800a8
akhileshiet <akhileshyadav310596@gmail.com>
2022-07-21 07:43:45 +0530

    day-2work

---
+++ Script/signin.js
@@ -0,0 +1,27 @@
+let arr = JSON.parse(localStorage.getItem("sign_up")) || [];
+//console.log(arrData);
+console.log(arr.length);
+
+document.querySelector("form").addEventListener("submit", signinFun);
+
+function signinFun() {
+  event.preventDefault();
+  if (arr.length == 0) {
+    alert("No user till now");
+    return;
+  }
+  let obj = {
+    perNmae: document.querySelector("#name").value,
+    perPass: document.querySelector("#password").value,
+  };
+  let res = false;
+  arr.map(function (ele) {
+    if (ele.personName === obj.perNmae && ele.password === obj.perPass) {
+      alert("log in successfull");
+    }
+    res = true;
+  });
+  if (res == false) {
+    alert("Please enter correct name and password ");
+  }
+}
